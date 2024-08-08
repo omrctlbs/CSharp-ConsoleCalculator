5112b99b9a321be109bbb732c7aab72630d553cf
Omer Catalbas <78023319+omrctlbs@users.noreply.github.com>
2024-08-08 14:06:24 +0300

    Add files via upload
    Calculator with if else/ else if

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//IF ELSE APPS
+
+//APP-1 CALCULATE BASED ON THE SELECTION OF 4 OPERATION TYPES FROM A MENU PRESENTED TO THE USER.
+
+Console.WriteLine("For addition, press (+)");
+Console.WriteLine("For subtraction, press (-)");
+Console.WriteLine("For multiplication, press (*)");
+Console.WriteLine("For division, press (/)");
+Console.WriteLine("Please make a selection");
+
+var operation = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("1st number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2nd number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+if (operation == "+")
+{
+    Console.WriteLine(number1 + number2);
+}
+else if (operation == "-")
+{
+    Console.WriteLine(number1 - number2);
+}
+else if (operation == "*")
+{
+    Console.WriteLine(number1 * number2);
+}
+else
+{
+    Console.WriteLine(number1 / number2);
+}
